c575db861374574309073127079911499cb35375
hitendramahajan <49238730+hitendramahajan@users.noreply.github.com>
2024-09-02 16:49:11 +0530

    Update image_op.py

--- image_op.py
+++ image_op.py
@@ -2,7 +2,7 @@ import cv2
 import imutils
 from Human_Detection import Detector
 
-img = cv2.imread('./testing/t4.jpg')
+img = cv2.imread('C:/IDE/Pedestrian_Detection_OpenCV/t2.jpg')
 img = imutils.resize(img, width=700)
 img = Detector(img)
 cv2.waitKey(0)
